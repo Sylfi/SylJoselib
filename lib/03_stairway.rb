b9c50e48923cd3e6214922679396c7544bfc1226
Syle <sylvain.firer@gmail.com>
2019-07-11 16:57:38 +0200

    meme chose

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,5 +1,3 @@
-
-
 def def_dice
     dice = rand(6)+1
     puts "la face du dé est #{dice}"
@@ -7,15 +5,15 @@ def def_dice
 end
 
 def roll(dice)
-
+    
     if dice == (5 || 6)
-       movement =  1
-
-    elsif
+        movement =  1
+        
+        elsif
         dice == (1)
-       movement = -1
-    else
-    #dice == (2 || 3 || 4)
+        movement = -1
+        else
+        #dice == (2 || 3 || 4)
         movement = 0
     end
     puts "le mouvement est de #{movement} case(s)"
@@ -23,7 +21,7 @@ def roll(dice)
 end
 
 def phrase_position(movement)
-
+    
     position = 0
     new_position = position + movement
     puts "ta position est de #{position}. Ta nouvelle position est de #{new_position} (#{position} + #{movement})"
@@ -34,23 +32,23 @@ def boucle(new_position)
     
     if new_position == 10
         puts "Tu as gagné, bravo petit fdp !"
-    else
-    puts "on relance les dés"
-
-    return def_dice
+        else
+        puts "on relance les dés"
+        
+        return boucle(new_position)
     end
 end
-        
 
 
-    def perform #loop position
-        dice = def_dice
-        #roll(dice)
-        movement = roll(dice)
-        #phrase_position(movement)
-        new_position = phrase_position(movement)
-        boucle(new_position)
-    end
+
+def perform
+    dice = def_dice
+    #roll(dice)
+    movement = roll(dice)
+    #phrase_position(movement)
+    new_position = phrase_position(movement)
+    boucle(new_position)
+end
 
 perform
 
