7c9f60fe66dc67f7150db15127a035816fd2b270
Syle <sylvain.firer@gmail.com>
2019-07-11 12:14:05 +0200

    derniere version de la pyramide chelou

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -1,21 +1,37 @@
 def full_pyramide
     puts "Salut, bienvenue dans ma super pyramide inversée ! Combien d’étages veux-tu ?"
-    nombre = gets.chomp.to_i
+    nombres = gets.chomp.to_i
+    nombre = nombres / 2
+    
+    
     if
-        nombre <1 || nombre >25
+        
+        nombres <1 || nombres >25
+        
         print "Tu n'as pas choisi un nombre entre 1 et 25 petit coquin! essaie encore !"
+        
         return full_pyramide
-    else
+        
+        elsif
+        
+        nombres % 2 == 0
+        
+        print "Choisi un nombre impair stp sinon tu vas MOURIR "
+        
+        return full_pyramide
+        
+        else
+    
     nombre.times {|n|
         print ' ' * (nombre - n)
         puts '#' * (2 * n + 1)
     }
-    end
+    puts '#' * ((nombres))
+    nombre.times {|n|
+        print ' ' * (n+1)
+        puts '#' * (((nombres) - (n * 2)) - 2)
+    }
+end
 end
 
 puts full_pyramide
-
-
-
-
-
